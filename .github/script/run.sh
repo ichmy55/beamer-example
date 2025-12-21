316d83faa2a53f4eb7019107302261fa66f5ae8a
ichmy55 <131500445+ichmy55@users.noreply.github.com>
2025-12-21 18:19:30 +0900

    Submodule 切り分け＃２ (#22)
    * change latex engine to lualatex
    
    * 2024-03-26
    
    * 2024-12-31-01
    
    * 2024-12-31-02
    
    * 2024-12-31-03
    
    * 2024-12-31-04
    
    * 2024-12-31-05
    
    * 2024-12-31-06
    
    * 2024-12-31-07
    
    * 2024-12-31-08
    
    * 2024-12-31-09
    
    * 2024-12-31-10
    
    * 2024-12-31-11
    
    * 2024-12-31-12
    
    * 2024-12-31-14
    
    * 2025-11-28
    
    * 2025-11-28-2
    
    * 2025-11-28-3
    
    * 2025-11-29
    
    * 2025-12-02
    
    * 2025-12-21
    
    * 2025-12-21-02
    
    ---------
    
    Co-authored-by: ichmy55 <ichmy@nifty.ne.jp>

--- .github/script/run.sh
+++ .github/script/run.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-
-set -eux
-
-npx textlint --format json "./src/**/*.tex" | python .github/script/convert.py >./report.json
-
-if [ -s ./report.json ]; then
-    cat ./report.json | reviewdog -f=rdjson -reporter=github-pr-review
-else
-    echo "No errors found"
-fi
+../../src/commons/github/script/run.sh
---
+++ variables.mk
@@ -0,0 +1,10 @@
+#
+# Docker コマンドマクロ
+#
+DOCKER_IMAGE  := ghcr.io/ichmy55/beamer-example/texcomp:main
+DOCKER_NAME   := beamer-example
+PACKAGE_USE   := 0              # 出来合いパッケージを使用するには"1"をセット
+#
+# 作成するスライド名
+#
+DEST_PDF := beamer-example
