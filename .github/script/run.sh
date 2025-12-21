844874695980126e668ae95e6a806d8d7a386a1b
ichmy55 <131500445+ichmy55@users.noreply.github.com>
2025-12-21 20:28:11 +0900

    Development (#28)
    * change latex engine to lualatex
    
    * 2024-03-26
    
    * 2024-12-31-01
    
    * 2024-12-31-02
    
    * 2024-12-31-03
    
    * 2024-12-31-04
    
    * 2024-12-31-05
    
    * 2024-12-31-06
    
    * 2024-12-31-07
    
    * 2024-12-31-08
    
    * 2024-12-31-09
    
    * 2024-12-31-10
    
    * 2024-12-31-11
    
    * 2024-12-31-12
    
    * 2024-12-31-14
    
    * 2025-11-28
    
    * 2025-11-28-2
    
    * 2025-11-28-3
    
    * 2025-11-29
    
    * 2025-12-02
    
    * 2025-12-21
    
    * 2025-12-21-02
    
    * 2025-12-21-06
    
    * 2025-12-21-07
    
    * 2025-12-21-08
    
    * 2025-12-21-09
    
    * 2025-12-21-10
    
    ---------
    
    Co-authored-by: ichmy55 <ichmy@nifty.ne.jp>

--- .github/script/run.sh
+++ .github/script/run.sh
@@ -1,1 +1,11 @@
-../../src/commons/github/script/run.sh
+#!/bin/sh
+
+set -eux
+
+npx textlint --format json "./src/**/*.tex" | python .github/script/convert.py >./report.json
+
+if [ -s ./report.json ]; then
+    cat ./report.json | reviewdog -f=rdjson -reporter=github-pr-review
+else
+    echo "No errors found"
+fi
