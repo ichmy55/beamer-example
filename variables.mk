5d3237b070fdcfeb6f9e75f722d65118efb0a481
ichmy55 <ichmy@nifty.com>
2025-12-28 10:49:03 +0900

    2025-12-28-08

--- variables.mk
+++ variables.mk
@@ -3,7 +3,7 @@
 #
 DOCKER_IMAGE  := ghcr.io/ichmy55/beamer-example/texcomp:main
 DOCKER_NAME   := beamer-example
-PACKAGE_USE   := 0              # 出来合いパッケージを使用するには"1"をセット
+PACKAGE_USE   := 1              # 出来合いパッケージを使用するには"1"をセット
 #
 # 作成するスライド名
 #
